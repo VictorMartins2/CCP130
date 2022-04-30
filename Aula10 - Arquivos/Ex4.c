5f6d2c61d276bf4e927c30f4aaa4f986496c3dae
Vinicius Nunes Martins <nunesmartinsvinicius@gmail.com>
2022-04-30 19:02:27 -0300

    Ex4

---
+++ Aula10 - Arquivos/Ex4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define VOWELS "aeiouAEIOU"
+#define CONSONANTS "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+#define MAX_LENGTH 255
+
+
+int main () {
+
+    while (1)
+    {
+        printf("Digite o nome do arquivo: ");
+        char *name[MAX_LENGTH]; scanf("%s", &name);
+        int vowels = 0, consonants = 0;
+
+        FILE *file = fopen(name, "r");
+
+
+        if (file == NULL) {
+            printf("Erro ao abrir o arquivo!\n");
+            continue;
+        }
+
+        char *line = NULL;
+        size_t len = 0;
+        while (getline(&line, &len, file) != -1) {
+            int i = 0;
+            while (line[i] != '\0') {
+                if (strchr(VOWELS, line[i]) != NULL) {
+                    ++vowels;
+                } else if (strchr(CONSONANTS, line[i]) != NULL) {
+                    ++consonants;
+                }
+                ++i;
+            }
+        }
+
+        printf("Vogais: %d\n", vowels);
+        printf("Consoantes: %d\n", consonants);
+        break;
+    }
+    
+
+    return 0;
+}
