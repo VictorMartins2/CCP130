b2f1431cb76ca2423635f4a80ddaf4521c589b0c
Victor Martins de Oliveira <victor27611@gmail.com>
2022-04-30 19:28:55 -0300

    Ex3

---
+++ Aula10 - Arquivos/Ex3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FILE_INPUT "numeros3.txt"
+#define FILE_OUTPUT "numeros3unicos.txt"
+
+void get_numbers();
+
+
+int main () {
+
+    return 0;
+}
+
+
+void get
+
