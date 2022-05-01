9ce43fc95f80c1ba862b123e8f96f4f570466026
Vinicius Nunes Martins <nunesmartinsvinicius@gmail.com>
2022-04-30 21:53:36 -0300

    Ex3

--- Aula10 - Arquivos/Ex3.c
+++ Aula10 - Arquivos/Ex3.c
@@ -4,15 +4,38 @@
 
 #define FILE_INPUT "numeros3.txt"
 #define FILE_OUTPUT "numeros3unicos.txt"
+#define TRUE 1
+#define FALSE 0
 
-void get_numbers();
+void insert_num(int num) {
+    FILE *file = fopen(FILE_OUTPUT, "w");
+    if (file == NULL){
+        printf("Erro ao abrir arquivo de saída.\n");
+        exit(1);
+    }
+    int flag = FALSE;
+    char *line = NULL;
+    size_t len = 0;
+    while (getline(&line, &len, file) != -1) {
+        if (num != atoi(line)) {
+            continue;
+        } else {
+            flag = TRUE;
+            break;
+        }
+    }
+    if (flag) {
+        fprintf(file, "%d\n", num);
+    }
+
+    fclose(file);
+}
 
 
 int main () {
-
     return 0;
 }
 
 
-void get
+
 
